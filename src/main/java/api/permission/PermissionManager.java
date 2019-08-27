cab04eea9de4ec3b42ee37f9defc02b557b83ed0
BuildTools <unconfigured@null.spigotmc.org>
2019-08-27 11:50:18 +0300

    оптимизировал базу данных

--- src/main/java/api/permission/PermissionManager.java
+++ src/main/java/api/permission/PermissionManager.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PermissionManager {
-    private static final HashMap<String, PermissionGroup> map = new HashMap<>();
+    private static final HashMap<String, PermissionGroup> lookup = new HashMap<>();
     static {
         addGroup(PermissionGroup.DEFAULT,"ПОЛЬЗОВАТЕЛЬ");
         addGroup(PermissionGroup.MODER, "МОДЕР", "roflanbot.settitle");
@@ -15,17 +15,17 @@ public class PermissionManager {
     }
     @Contract(pure = true)
     public static Map<String, PermissionGroup> getLookup() {
-        return map;
+        return lookup;
     }
 
     public static PermissionGroup getPermGroup(String name) {
-        return map.get(name);
+        return lookup.get(name);
     }
 
     public static final void addGroup(String name, String prefix, String... permissions) {
         PermissionGroup group = new PermissionGroup(Sets.newHashSet(permissions));
         group.setSuffix(prefix);
-        map.put(name, group);
+        lookup.put(name, group);
     }
 }
 
