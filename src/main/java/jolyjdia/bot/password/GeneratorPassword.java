e161316460d588616a12263ad0972ed930a64587
BuildTools <unconfigured@null.spigotmc.org>
2019-09-19 11:53:00 +0300

    добавил КД на команды

--- src/main/java/jolyjdia/bot/password/GeneratorPassword.java
+++ src/main/java/jolyjdia/bot/password/GeneratorPassword.java
@@ -1,12 +1,34 @@
 package jolyjdia.bot.password;
 
 import api.command.RegisterCommandList;
+import api.event.EventLabel;
+import api.event.Listener;
+import api.event.RegisterListEvent;
+import api.event.messages.SendCommandEvent;
 import api.utils.JavaModule;
+import org.jetbrains.annotations.NotNull;
 
-public class GeneratorPassword extends JavaModule {
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class GeneratorPassword extends JavaModule implements Listener {
+    private final Map<Integer, Long> cooldown = new WeakHashMap<>();
 
     @Override
     public final void onLoad() {
         RegisterCommandList.registerCommand(new PasswordCommand());
+        RegisterListEvent.registerEvent(this);
+    }
+    @EventLabel
+    public final void onCommand(@NotNull SendCommandEvent e) {
+        int userId = e.getUser().getUserId();
+        if(cooldown.containsKey(userId) && cooldown.get(userId) > System.currentTimeMillis()) {
+            e.getUser().sendMessageFromHisChat("Подождите 3 секунд, перед тем, как использовать команду снова");
+            e.setCancelled(true);
+            return;
+        } else {
+            cooldown.remove(userId);
+        }
+        cooldown.put(userId, System.currentTimeMillis() + 3000);
     }
 }
