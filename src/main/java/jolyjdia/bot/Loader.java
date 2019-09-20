a0155b736d0bb4db1f3a0d0c99ad79a5f6fc8ef8
BuildTools <unconfigured@null.spigotmc.org>
2019-09-20 13:08:34 +0300

    cleanup

--- src/main/java/jolyjdia/bot/Loader.java
+++ src/main/java/jolyjdia/bot/Loader.java
@@ -9,11 +9,9 @@ import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.groups.LongPollSettings;
 import jolyjdia.bot.calculate.CalculatorRegister;
-import jolyjdia.bot.geo.GeoLoad;
 import jolyjdia.bot.kubanoid.KubanoidLoad;
 import jolyjdia.bot.password.GeneratorPassword;
 import jolyjdia.bot.puzzle.Puzzle;
-import jolyjdia.bot.shoutbox.ShoutboxMain;
 import jolyjdia.bot.translator.YandexTraslate;
 import org.jetbrains.annotations.Contract;
 
@@ -45,9 +43,9 @@ public class Loader {
     private static void registerAll() {
         new CalculatorRegister().onLoad();
         new YandexTraslate().onLoad();
-        new GeoLoad().onLoad();
+       // new GeoLoad().onLoad();
         new Puzzle().onLoad();
-        new ShoutboxMain().onLoad();
+        //new ShoutboxMain().onLoad();
         new KubanoidLoad().onLoad();
         new GeneratorPassword().onLoad();
     }
