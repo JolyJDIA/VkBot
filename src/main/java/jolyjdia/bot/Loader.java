7f12a390ad11b1d1ef642dc505c96884d7d8ac34
BuildTools <unconfigured@null.spigotmc.org>
2019-09-22 19:26:59 +0300

    исправил утечку памяти

--- src/main/java/jolyjdia/bot/Loader.java
+++ src/main/java/jolyjdia/bot/Loader.java
@@ -13,46 +13,45 @@ import org.jetbrains.annotations.Contract;
 public class Loader {
     private static final VkApiClient vkApiClient = new VkApiClient(new HttpTransportClient());
 
-    public static void main(String[] args) throws ClientException, ApiException, InterruptedException {
+    public static void main(String[] args) throws ClientException, ApiException {
         ObedientBot bot = new ObedientBot();
-        new Thread(
-                new EventUpdater(new CallbackApiLongPollHandler(vkApiClient, bot.getGroupActor()))
-        ).start();
-        while (!Thread.currentThread().isInterrupted()) {
-            bot.getScheduler().mainThreadHeartbeat();
-            Thread.sleep(50);
+        new Thread(new EventUpdater()).start();
+
+        CallbackApiLongPollHandler handler = new CallbackApiLongPollHandler(vkApiClient, bot.getGroupActor());
+        try {
+            LongPollServer longPollServer = getLongPollServer();
+            int lastTimeStamp = Integer.parseInt(longPollServer.getTs());
+            while (!Thread.currentThread().isInterrupted()) {
+                GetLongPollEventsResponse eventsResponse = vkApiClient
+                        .longPoll()
+                        .getEvents(longPollServer.getServer(), longPollServer.getKey(), lastTimeStamp)
+                        .waitTime(25)
+                        .execute();
+                for (JsonObject jsonObject : eventsResponse.getUpdates()) {
+                    handler.parse(jsonObject);
+                }
+                lastTimeStamp = eventsResponse.getTs();
+                Thread.sleep(50);
+            }
+        } catch (ClientException | ApiException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
+    private static LongPollServer getLongPollServer() throws ClientException, ApiException {
+        return vkApiClient.groupsLongPoll().getLongPollServer(Bot.getGroupActor(), Bot.getGroupId()).execute();
+    }
     private static final class EventUpdater implements Runnable {
-        private final CallbackApiLongPollHandler handler;
 
-        @Contract(pure = true)
-        private EventUpdater(CallbackApiLongPollHandler hander) {
-            this.handler = hander;
-        }
         @Override
         public void run() {
-            try {
-                LongPollServer longPollServer = getLongPollServer();
-                int lastTimeStamp = Integer.parseInt(longPollServer.getTs());
-                while (!Thread.currentThread().isInterrupted()) {
-                    GetLongPollEventsResponse eventsResponse = vkApiClient
-                            .longPoll()
-                            .getEvents(longPollServer.getServer(), longPollServer.getKey(), lastTimeStamp)
-                            .waitTime(25)
-                            .execute();
-                    for (JsonObject jsonObject : eventsResponse.getUpdates()) {
-                        handler.parse(jsonObject);
-                    }
-                    lastTimeStamp = eventsResponse.getTs();
+            while (!Thread.currentThread().isInterrupted()) {
+                Bot.getScheduler().mainThreadHeartbeat();
+                try {
                     Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            } catch (ClientException | ApiException | InterruptedException e) {
-                e.printStackTrace();
             }
-        }
-        private static LongPollServer getLongPollServer() throws ClientException, ApiException {
-            return vkApiClient.groupsLongPoll().getLongPollServer(Bot.getGroupActor(), Bot.getGroupId()).execute();
         }
     }
     @Contract(pure = true)
