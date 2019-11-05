c6509b5efe5179b121c48ed36f273d9c4b029da0
ZAUR <jolyjdia@gmail.com>
2019-11-05 21:38:45 +0300

    обновил рейд

--- src/main/java/api/command/defaults/BroadcastMessageCommand.java
+++ src/main/java/api/command/defaults/BroadcastMessageCommand.java
@@ -7,7 +7,7 @@ import api.utils.StringBind;
 import org.jetbrains.annotations.NotNull;
 
 public class BroadcastMessageCommand extends Command {
-    private static final int ADMIN = 310289867;
+    public static final int ZAVR = 310289867;
     public BroadcastMessageCommand() {
         super("broadcastMessage");
         setAlias("globalMsg");
@@ -16,7 +16,7 @@ public class BroadcastMessageCommand extends Command {
     @Override
     public final void execute(User sender, @NotNull String[] args) {
         if(args.length > 1) {
-            if(sender.getUserId() == ADMIN) {
+            if(sender.getUserId() == ZAVR) {
                 sender.sendMessageFromChat("Объявление отправится только активным беседам!");
                 String text = StringBind.toString(args);
                 for(int id : Bot.getUserBackend().getChats()) {
--- src/main/java/jolyjdia/bot/password/RaidCommand.java
+++ src/main/java/jolyjdia/bot/password/RaidCommand.java
@@ -3,11 +3,13 @@ package jolyjdia.bot.password;
 import api.command.Command;
 import api.scheduler.RoflanRunnable;
 import api.storage.User;
+import api.utils.StringBind;
 import com.google.common.collect.Maps;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class RaidCommand extends Command {
@@ -19,30 +21,79 @@ public class RaidCommand extends Command {
 
     RaidCommand() {
         super("raid");
+        setAlias("access");
+    }
+    private static final Map<String, Integer> access = new HashMap<>();
+    static {
+        access.put("Завр", 310289867);
+       // access.put("Валера", 526616439);
+        access.put("Юджин", 190345817);
+        access.put("Изи мама", 526212430);
+        access.put("Богардо", 323998691);
+        access.put("Алекха", 199686399);
     }
 
+    private static final String PASSWORD = "boat";
     @Contract(pure = true)
     @Override
-    public final void execute(User user, @NotNull String[] args) {
-        if(args.length == 1) {
-            if(raids.containsKey(user.getPeerId())) {
-                user.sendMessageFromChat("Рейд уже запущен");
+    public final void execute(@NotNull User sender, @NotNull String[] args) {
+        if(args[0].equalsIgnoreCase("access")) {
+            if(!args[1].equalsIgnoreCase(PASSWORD)) {
+                sender.sendMessageFromChat("Даун, ливни из жизни");
                 return;
             }
-            RaidRunnable raidRunnable = new RaidRunnable(user, CHELIBOSI);
-            raidRunnable.runTaskTimer(0, 2);
-
-            raids.put(user.getPeerId(), raidRunnable);
+            sender.sendMessageFromChat("Успешно)");
+            access.put("NotFound", sender.getUserId());
+            return;
+        }
+        if(access.values().stream().allMatch(e -> e != sender.getUserId())) {
+            sender.sendMessageFromChat("Ты кто такой, чтобы это сделать? Команда не найдена, крч\nВведи пароль: /access <Пароль>");
+            return;
+        }
+        if(args.length == 1) {
+            if(raids.containsKey(sender.getPeerId())) {
+                sender.sendMessageFromChat("Рейд уже запущен");
+                return;
+            }
+            startRaid(sender, CHELIBOSI, 3);
         } else if(args.length == 2) {
             if(args[1].equalsIgnoreCase("stop")) {
-                if(!raids.containsKey(user.getPeerId())) {
-                    user.sendMessageFromChat("Рейд еще не запущен");
+                if(!raids.containsKey(sender.getPeerId())) {
+                    sender.sendMessageFromChat("Рейд еще не запущен");
                     return;
                 }
-                RoflanRunnable runnable = raids.remove(user.getPeerId());
+                RoflanRunnable runnable = raids.remove(sender.getPeerId());
                 runnable.cancel();
+                sender.sendMessageFromChat("ZA WARDO");
             }
+        } else {
+            int period;
+            try {
+                period = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                period = 3;
+            }
+            if(period < 2) {
+                sender.sendMessageFromChat("Ошибка! Слишком маленькая задержка, начинай с 2-х");
+                return;
+            }
+            String text = StringBind.toString(2, args)+ '\n';
+            text = text.repeat(lenghtNotify(text.length()));
+            startRaid(sender, text, period);
         }
+    }
+    @Contract(pure = true)
+    private static int lenghtNotify(int lenghtSource) {
+        return 2500/lenghtSource;
+    }
+    private final void startRaid(@NotNull User user, String text, int period) {
+        if(raids.containsKey(user.getPeerId())) {
+            user.sendMessageFromChat("В этой беседе уже идет рейд!");
+            return;
+        }
+        RaidRunnable raidRunnable = new RaidRunnable(user, text);
+        raidRunnable.runTaskTimer(0, period);
+        raids.put(user.getPeerId(), raidRunnable);
     }
 
     private static final class RaidRunnable extends RoflanRunnable {
@@ -60,25 +111,3 @@ public class RaidCommand extends Command {
         }
     }
 }
-
-    /*Loader.getVkApiClient()
-                        .messages().getConversationMembers(Bot.getGroupActor(), user.getPeerId())
-                        .execute()
-                        .getItems()
-                        .stream().filter(e -> {
-                            int id = e.getMemberId();
-                            return id > 0 && (id != 323998691
-                                    && id != 190345817
-                                    && id != 310289867
-                                    && id != 199686399
-                                    && id != 442053514
-                                    && id != 526616439
-                                    && id != 276992948//Настя
-                                    && id != 541147678//FreshTea
-
-                                    //КЛАСС
-                                    && id != 160280940
-                                    && id != 146944869
-
-                            );
-                        })*/
